7be718c2a9d35074571351edc41f54dc06cb70a0
Omar Azzat <oazzat19@gmail.com>
2018-09-06 06:54:19 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,29 @@
+function forLoop(array){
+  for(let i=0;i<25;i++){
+    if (i===1){
+      array.push("I am 1 strange loop.")
+      
+    }
+    else{
+      array.push(`I am ${i} strange loops.`)
+      
+    }
+    
+  }
+  return array
+}
+
+function whileLoop(num){
+  while(num!==0){
+  console.log(num)
+  num--
+  }
+  return "done"
+}
+
+function doWhileLoop(array){
+  do{
+    delete array[array.length-1]
+  }
+  while(array.length>0)
+}
